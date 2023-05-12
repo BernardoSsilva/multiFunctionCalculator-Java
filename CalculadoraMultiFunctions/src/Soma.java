c55aec17e554bb31be2c7403c5a7955a9408568a
Bernardo <126777966+BernardoSsilva@users.noreply.github.com>
2023-05-12 11:41:02 -0300

    Add files via upload

---
+++ CalculadoraMultiFunctions/src/Soma.java
@@ -0,0 +1,28 @@
+package src;
+
+import java.util.Scanner;
+
+public class Soma {
+    public void realizarSoma(){
+        float num1, num2;
+        String check;
+        Scanner scan = new Scanner(System.in);
+        boolean continuar = true;
+        while(continuar){
+            System.out.print("Digite o primeiro numero: ");
+            num1 = scan.nextFloat();
+            System.out.println(" ");
+            System.out.print("Digite o segundo numero: ");
+            num2 = scan.nextFloat();
+            System.out.println(" ");
+            System.out.print("O resultado é: "+(num1+num2));
+            System.out.println(" ");
+            System.out.println("Deseja continuar? (S/N): ");
+            check = scan.next().toUpperCase();
+            System.out.println(" ");
+            if (check.equals("N")){
+                continuar = false;
+            }
+        }
+    } 
+}
