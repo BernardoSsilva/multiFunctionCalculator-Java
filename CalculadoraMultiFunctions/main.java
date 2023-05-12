379cac25eb6eb7a1367128f34a120a62fc6f9199
Bernardo <126777966+BernardoSsilva@users.noreply.github.com>
2023-05-12 11:26:44 -0300

    Project create

---
+++ CalculadoraMultiFunctions/main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class main{
+    public static void main(String[] args) {
+        int value;
+        boolean continuar = true;
+        Scanner scan = new Scanner(System.in);        
+        System.out.println("Calculadora Multifunções");
+        System.out.println(" ");
+        System.out.println("============ ");
+        System.out.println(" ");
+        System.out.println("Digite 1 para realizar soma: ");
+        System.out.println(" ");
+        System.out.println("Digite 2 para realizar subtração: ");
+        System.out.println(" ");
+        System.out.println("Digite 3 para realizar multiplicação: ");
+        System.out.println(" ");
+        System.out.println("Digite 4 para realizar divisão: ");
+        System.out.println(" ");
+        System.out.println("Digite 5 para teorema de pitagoras: ");
+        System.out.println(" ");
+        System.out.println("Digite 6 para bhaskara: ");
+        System.out.println(" ");
+        System.out.println("Digite 7 para calcular raiz quadrada: ");
+        System.out.println(" ");
+        System.out.println("Digite 8 para exponenciação: ");
+        System.out.println(" ");
+        System.out.println("Digite 9 para encontrar a raiz cubica: ");
+        System.out.println(" ");
+        System.out.println("Digite 10 para calcular area: ");
+        System.out.println(" ");
+        System.out.println("Digite 11 para calcular o perimetro de uma circunferencia: ");
+        System.out.println(" ");
+        System.out.println("Digite 12 para calcular velocidade media: ");
+    }
+}
